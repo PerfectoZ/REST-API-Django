09a73948c2b2727ba1e99cb0e168a6fa06633098
PerfectoZ <mandeeptaneja21@outlook.com>
2023-01-28 12:25:36 +0530

    Restrict Feed to Logged in User only

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -9,7 +9,7 @@ from profiles_api import permissions
 from rest_framework import filters
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated #OrReadOnly
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -117,7 +117,7 @@ class UserProfileFeedViewSet(viewsets.ModelViewSet):
     queryset = models.ProfileFeedItem.objects.all()
     permission_classes = (
         permissions.UpdateOwnStatus,
-        IsAuthenticatedOrReadOnly
+        IsAuthenticated #OrReadOnly
     )
 
     def perform_create(self, serializer):
